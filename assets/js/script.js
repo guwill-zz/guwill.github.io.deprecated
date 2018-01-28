ce7ce6d40a4286a957167f112bbcc1e8d4d343a5
Will Gu <3182426+guwill@users.noreply.github.com>
2018-01-28 03:43:21 -0500

    Replace carousel with parallax scrolling (#1)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
-$(function(){
-    $('.carousel').carousel({
-      interval: 10
-    });
-});
+( function( $ ) {
+	$window = $(window);
+	$slide = $('.slide');
+
+	height = $window.height();
+
+	$slide.height();
+
+	$window.scroll(function() {
+		if ($(this).scrollTop() > 200) {
+			$('#box').fadeOut();
+		} else {
+			$('#box').fadeIn();
+		}
+	});
+
+} )( jQuery );
